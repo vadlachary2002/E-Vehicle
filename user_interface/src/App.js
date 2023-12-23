d620e0881c9848727b24324ff181ed9e7ef7bb7a
vadlachary2002 <vadlanagarajuchary2002@gmail.com>
2023-12-24 01:35:23 +0530

    test

--- user_interface/src/App.js
+++ user_interface/src/App.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
 import { Home, Login, Profile, Register, Search } from './pages';
